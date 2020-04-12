1f4cb939407bcfa7266f8ce536e6869252f77af1
Zbigniew Malcherczyk <zmalcherczyk@gmail.com>
2020-04-12 23:22:25 +0200

    Symfony services config in PHP (#7)

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Controller;
+namespace Ferror\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 
--- tests/Controller/DefaultControllerTest.php
+++ tests/Controller/DefaultControllerTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace App\Tests\Controller;
+namespace Ferror\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
