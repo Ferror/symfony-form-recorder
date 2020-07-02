ff8337f9e5d47e9e8b850cf8a9ef9b80731ecc83
ferror <zmalcherczyk@gmail.com>
2020-07-02 22:59:18 +0200

    remove import

--- src/Controller/SubmitAction.php
+++ src/Controller/SubmitAction.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Ferror\Controller;
 
 use Ferror\FormRecorder\Exception\HoneyPotFoundException;
-use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
